57cd60588eea18c81d948225a8cbcdad31c94d53
dima <zhuikovda@gmail.com>
2023-01-09 14:23:57 +0300

    HW5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Error404 from './pages/Error404'
-import PreJunior from './pages/PreJunior'
-import Junior from './pages/Junior'
-import JuniorPlus from './pages/JuniorPlus'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Error404 from "./pages/Error404";
+import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
-}
+    PRE_JUNIOR: "/pre-junior",
+    JUNIOR: "/junior",
+    JUNIOR_PLUS: "/junior-plus"
+};
 
 function Pages() {
     return (
@@ -21,15 +21,15 @@ function Pages() {
                 {/*<Route ...*/}
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path="/pre-junior" element={<PreJunior />} />
+                <Route path="/junior" element={<Junior />} />
+                <Route path="/junior-plus" element={<JuniorPlus />} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route element={<JuniorPlus />} />
             </Routes>
         </div>
-    )
+    );
 }
 
-export default Pages
+export default Pages;
