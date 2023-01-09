cbdbf24a9354ed3379954478088878fc5d9f74bc
dima <zhuikovda@gmail.com>
2023-01-09 15:09:10 +0300

    fix bug page-404

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path="/junior-plus" element={<JuniorPlus />} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path="/404" element={<JuniorPlus />} />
+                <Route path="/404" element={<Error404 />} />
             </Routes>
         </div>
     );
