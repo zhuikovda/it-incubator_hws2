337a653f96736ae29fdf0f5698a0e342372d790a
dima <zhuikovda@gmail.com>
2023-01-28 22:08:41 +0300

    HW8 logics

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { UserType } from './HW8'
-import s from './HW8.module.css'
+import React from "react";
+import { UserType } from "./HW8";
+import s from "./HW8.module.css";
 
 // types
 type UserPropsType = {
-    u: UserType
-}
+    u: UserType;
+};
 
 const User: React.FC<UserPropsType> = ({ u }) => {
     return (
-        <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
-            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
+        <tr id={"hw8-user-" + u._id + "-" + u.age} className={s.item}>
+            <td id={"hw8-user-name-" + u._id} className={s.nameCol}>
                 {/*отобразить имя*/}
-
+                {u.name}
             </td>
-            <td id={'hw8-user-age-' + u._id}>
+            <td id={"hw8-user-age-" + u._id}>
                 {/*отобразить возраст*/}
-
+                {u.age}
             </td>
         </tr>
-    )
-}
+    );
+};
 
-export default User
+export default User;
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,22 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+    // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case "sort": {
+            // by name
 
-            return state // need to fix
+            return action.payload === "up"
+                ? state.sort((a, b) => (a.name < b.name ? -1 : 1))
+                : state.sort((a, b) => (a.name < b.name ? 1 : -1)); // need to fix
         }
-        case 'check': {
 
-            return state // need to fix
+        case "check": {
+            return state.filter((el) => el.age >= action.payload); // need to fix
         }
         default:
-            return state
+            return state;
     }
-}
+};
